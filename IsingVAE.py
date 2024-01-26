649a48a597123992cf3a6a539d86ceb0d95baffd
JoeHadley <148891416+JoeHadley@users.noreply.github.com>
2024-01-26 14:30:44 +0000

    Update IsingVAE.py

--- IsingVAE.py
+++ IsingVAE.py
@@ -1,174 +1,152 @@
-import tensorflow as tf
+from tensorflow import keras
+from keras.layers import Conv2D, Conv2DTranspose, Input, Flatten, Dense, Lambda, Reshape
+from keras.models import Model
+from keras.datasets import mnist
+from keras import backend as K
+import numpy as np
 import matplotlib.pyplot as plt
 
-import os
-from google.colab import files
-import numpy as np
 
-# Change the current working directory to the folder
-os.chdir('cropped')
-uploaded = files.upload()
-
-# preprocess
-image_size = 28
-
-def preprocess(image):
-    image = tf.io.read_file(image)
-    image = tf.io.decode_jpeg(image)
-    image = tf.cast(image, tf.float32)
-    image = tf.image.resize(image, (image_size, image_size))
-    image = image / 255.0
-    #image = tf.reshape(image, shape = (image_size, image_size, 3,))
-    return image
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
 
 
-# build the model
+# Normalise
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train = x_train/255
+x_test = x_test/255
 
-latent_dim = 512
-from keras.models import Sequential, Model
+# Reshape
+img_width = x_train.shape[1]
+img_height = x_train.shape[2]
+num_channels = 1
+x_train = x_train.reshape(x_train.shape[0],img_height,img_width,num_channels)
+x_test = x_test.reshape(x_test.shape[0],img_height,img_width,num_channels)
+input_shape = (img_height, img_width, num_channels)
 
-from keras.layers import Dense, Conv2D, Conv2DTranspose, Input, Flatten, BatchNormalization, Lambda, Reshape, Activation
-from keras.layers import LeakyReLU
-from keras.activations import selu
-from keras.layers import Multiply, Add
-from keras.optimizers import Adam
 
-from keras import backend as K
 
-K.clear_session()
+# Eager execution hack
+from tensorflow.python.framework.ops import disable_eager_execution
+disable_eager_execution()
 
-# Build encoder
-encoder_input = Input(shape=(28, 28, 1))
 
-x = Conv2D(28, kernel_size=5, activation=LeakyReLU(0.02), strides=1, padding='same')(encoder_input)
-x = BatchNormalization()(x)
+# Build Model
 
-filter_size = [64, 128, 256, 512]
-for i in filter_size:
-    x = Conv2D(i, kernel_size=5, activation=LeakyReLU(0.02), strides=2, padding='same')(x)
-    x = BatchNormalization()(x)
+latent_dim = 2
 
+input_img = Input(shape=input_shape, name='encoder_input')
+x = Conv2D(32, 3, padding = 'same', activation = 'relu')(input_img)
+x = Conv2D(64, 3, padding = 'same', activation = 'relu',strides = (2, 2))(x)
+x = Conv2D(64, 3, padding = 'same', activation = 'relu')(x)
+x = Conv2D(64, 3, padding = 'same', activation = 'relu')(x)
+
+conv_shape = K.int_shape(x) # Shape of con to be provided to decoder
+
+# Flatten
 x = Flatten()(x)
-x = Dense(1024, activation='selu')(x)
-encoder_output = BatchNormalization()(x)
+x = Dense(32, activation = 'relu')(x)
 
-# sampling layer
-mu = Dense(latent_dim)(encoder_output)
-log_var = Dense(latent_dim)(encoder_output)
+z_mu = Dense(latent_dim, name = 'latent_mu')(x)
+z_sigma = Dense(latent_dim, name = 'latent_sigma')(x)
 
-def sampling(args):
-    mu, log_var = args
-    batch = K.shape(mu)[0]
-    dim = K.int_shape(mu)[1]
-    epsilon = K.random_normal(shape=(batch, dim))
-    return mu + K.exp(0.5 * log_var) * epsilon
 
-z = tf.keras.layers.Lambda(sampling, output_shape=(latent_dim,))([mu, log_var])
 
-encoder = Model(encoder_input, outputs=[mu, log_var, z], name='encoder')
-encoder.summary()
 
-# Build the decoder
-decoder = Sequential()
-decoder.add(Dense(1024, activation='selu', input_shape=(latent_dim,)))
-decoder.add(BatchNormalization())
+# Reparametrization Trick
 
-decoder.add(Dense(8192, activation='selu'))
-decoder.add(Reshape((4, 4, 512)))
+def sample_z(args):
+  z_mu, z_sigma = args
+  eps = K.random_normal(shape=(K.shape(z_mu)[0],K.int_shape(z_mu)[1]))
+  return z_mu + K.exp(z_sigma/2)*eps
 
-# Adjust the transpose convolutional layers for 28x28x1
-decoder.add(Conv2DTranspose(256, (5, 5), activation=LeakyReLU(0.02), strides=2, padding='same'))
-decoder.add(BatchNormalization())
+z = Lambda(sample_z, output_shape=(latent_dim,),name = 'z')([z_mu,z_sigma])
 
-decoder.add(Conv2DTranspose(128, (5, 5), activation=LeakyReLU(0.02), strides=2, padding='same'))
-decoder.add(BatchNormalization())
+encoder = Model(input_img, [z_mu, z_sigma, z], name = 'encoder')
+print(encoder.summary())
 
-decoder.add(Conv2DTranspose(64, (5, 5), activation=LeakyReLU(0.02), strides=2, padding='same'))
-decoder.add(BatchNormalization())
 
-decoder.add(Conv2DTranspose(32, (5, 5), activation=LeakyReLU(0.02), strides=2, padding='same'))
-decoder.add(BatchNormalization())
+decoder_input = Input(shape = (latent_dim, ), name = 'decoder_input')
 
-# Adjust the number of filters in the last layer to match the single channel (greyscale)
-decoder.add(Conv2DTranspose(1, (5, 5), activation='sigmoid', strides=1, padding='same'))
-decoder.add(BatchNormalization())
+x = Dense(conv_shape[1]*conv_shape[2]*conv_shape[3], activation = 'relu')(decoder_input)
 
+x = Reshape((conv_shape[1], conv_shape[2], conv_shape[3]))(x)
+
+
+x = Conv2DTranspose(32, 3, padding = 'same', activation = 'relu', strides = (2,2))(x)
+
+x = Conv2DTranspose(num_channels, 3, padding = 'same', activation = 'sigmoid', name = 'decoder_output')(x)
+
+decoder = Model(decoder_input,x,name = 'decoder')
 decoder.summary()
 
-# make loss function 
-# vae loss = reconstruction loss + KL div
-
-def reconstruction_loss(y, y_pred):
-    return tf.reduce_mean(tf.square(y - y_pred))
-
-def kl_loss(mu, log_var):
-    loss = -0.5 * tf.reduce_mean(1 + log_var - tf.square(mu) - tf.exp(log_var))
-    return loss
-
-def vae_loss(y_true, y_pred, mu, log_var):
-    return reconstruction_loss(y_true, y_pred) + (1 / (28*28)) * kl_loss(mu, log_var)
+z_decoded = decoder(z)
 
 
-# combine encoder and decoder
-mu, log_var, z = encoder(encoder_input)
-reconstructed = decoder(z)
-model = Model(encoder_input, reconstructed, name ="vae")
-loss = kl_loss(mu, log_var)
-model.add_loss(loss)
-model.summary()
+# Custom Loss
+class CustomLayer(keras.layers.Layer):
+
+    def vae_loss(self, x, z_decoded):
+        x = K.flatten(x)
+        z_decoded = K.flatten(z_decoded)
+        
+        # Reconstruction loss (as we used sigmoid activation we can use binarycrossentropy)
+        recon_loss = keras.metrics.binary_crossentropy(x, z_decoded)
+        
+        # KL divergence
+        kl_loss = -5e-4 * K.mean(1 + z_sigma - K.square(z_mu) - K.exp(z_sigma), axis=-1)
+        #kl_loss = -0.5 * K.prod(K.sum(1 + z_sigma - K.square(z_mu) - K.exp(z_sigma), axis=-1))
+
+        return K.mean(recon_loss + kl_loss)
+
+    # add custom loss to the class
+    def call(self, inputs):
+        x = inputs[0]
+        z_decoded = inputs[1]
+        loss = self.vae_loss(x, z_decoded)
+        self.add_loss(loss, inputs=inputs)
+        return x
 
 
-# make a function to save images while learning
-def save_images(model, epoch, step, input_):
-    prediction = model.predict(input_)
-    fig, axes = plt.subplots(5,5, figsize = (14,14))
-    idx = 0
-    for row in range(5):
-        for column in range(5):
-            image = prediction[idx] * 255
-            image = image.astype("int32")
-            axes[row, column].imshow(image)
-            axes[row, column].axis("off")
-            idx+=1
-    output_path = "output/"
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-    plt.savefig(output_path + "Epoch_{:04d}_step_{:04d}.jpg".format(epoch, step))
-    plt.close()
+y = CustomLayer()([input_img, z_decoded])
 
-# train vae
 
-from keras.optimizers import Adam
 
-random_vector = tf.random.normal(shape = (25, latent_dim,))
-save_images(decoder, 0, 0, random_vector)
+# VAE 
 
-mse_losses = []
-kl_losses = []
+vae = Model(input_img, y, name='vae')
 
-optimizer = Adam(0.0001, 0.5)
-epochs = 1
+# Compile VAE
+vae.compile(optimizer='adam', loss=None)
+vae.summary()
 
-for epoch in range(1, epochs + 1):
-    print("Epoch: ", epoch)
-    for step, training_batch in enumerate(training_dataset):
-        with tf.GradientTape() as tape:
-            reconstructed = model(training_batch)
-            y_true = tf.reshape(training_batch, shape = [-1])
-            print(y_true)
-            y_pred = tf.reshape(reconstructed, shape = [-1])
-            print(y_pred)
-            mse_loss = reconstruction_loss(y_true, y_pred)
-            mse_losses.append(mse_loss.numpy())
-            
-            kl = sum(model.losses)
-            kl_losses.append(kl.numpy())
-            
-            train_loss = 0.01 * kl + mse_loss
-            
-            grads = tape.gradient(train_loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(grads, model.trainable_variables))
-            
-            if step % 10 == 0:
-                save_images(decoder, epoch, step, random_vector)
-            print("Epoch: %s - Step: %s - MSE loss: %s - KL loss: %s" % (epoch, step, mse_loss.numpy(), kl.numpy()))
+# Train autoencoder
+vae.fit(x_train, None, epochs = 10, batch_size = 32, validation_split = 0.2)
+#
+
+
+# Visualize results
+
+#Visualize inputs mapped to the Latent space
+#Remember that we have encoded inputs to latent space dimension = 2. 
+#Extract z_mu --> first parameter in the result of encoder prediction representing mean
+
+mu, _, _ = encoder.predict(x_test)
+#Plot dim1 and dim2 for mu
+plt.figure(figsize=(10, 10))
+plt.scatter(mu[:, 0], mu[:, 1], c=y_test, cmap='brg')
+plt.xlabel('dim 1')
+plt.ylabel('dim 2')
+plt.colorbar()
+plt.show()
+
+
+# Visualize images
+#Single decoded image with random input latent vector (of size 1x2)
+#Latent space range is about -5 to 5 so pick random values within this range
+#Try starting with -1, 1 and slowly go up to -1.5,1.5 and see how it morphs from 
+#one image to the other.
+sample_vector = np.array([[1,-1]])
+decoded_example = decoder.predict(sample_vector)
+decoded_example_reshaped = decoded_example.reshape(img_width, img_height)
+plt.imshow(decoded_example_reshaped)
