a5db24bf42dfc016fd939a2a01d6d0ac1983bcc7
JoeHadley <jjhadley@liverpool.ac.uk>
2026-01-14 21:01:08 +0000

    Testing updates

--- VAEJacobian.py
+++ VAEJacobian.py
@@ -3,15 +3,29 @@ import torch
 
 
 
-    super(VAE, self).__init__()
-    self.device = device   
-    self.beta = beta
-    self.window_dim = window_dim
-    self.hidden_dim = hidden_dim
-    self.latent_dim = latent_dim
-    self.double_input = double_input
-      
 
 
-myVAE = VAE(window_dim=16, hidden_dim=8, latent_dim=2, double_input=True, device='cpu', VAEbeta=1.0)
+
+
+myVAE = VAE(window_dim=16, hidden_dim=8, latent_dim=2, double_input=False, device='cpu', beta=1.0)
+
+
+input_phi = torch.randn(1, 16)
+print("Input phi:", input_phi)
+
+
+
+mean, logvar = myVAE.encode(input_phi)
+print("Encoded mean:", mean)
+print("logvar:", logvar)
+z_reparam = myVAE.reparameterization(mean, logvar)
+print("Reparameterized z:", z_reparam)
+
+output_phi = myVAE.decode(z_reparam, input_phi=input_phi)
+
+backMean, backLogvar = myVAE.encode(output_phi)
+print("Decoded output phi:", output_phi)
+print("Re-encoded mean from output phi:", backMean)
+print("Re-encoded logvar from output phi:", backLogvar)
+
 
